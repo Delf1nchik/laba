91104481b9a017e64b561836873b399f999d8f06
Данченко Артемий <artemferz@icloud.com>
2024-10-05 09:11:33 +0300

    Initial commit

--- time.pas
+++ time.pas
@@ -1,5 +1,5 @@
 ﻿##
 var n := readinteger('Введите год: ');
-if (n mod 4 = 0) and (n mod 100 <> 0) or (n mod 400 = 0) then print('Високосный')
+if (n mod 4 = 0) and (n mod 100 <> 0) or (n mod 400 = 0) then print('366')
 else
-  print('Невисокосный'); 
+  print('365'); 
