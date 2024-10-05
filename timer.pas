3117769568a7a5d171d6fff3fecde9bdc31cdff4
Данченко Артемий <artemferz@icloud.com>
2024-10-05 09:18:32 +0300

    Initial commit

---
+++ timer.pas
@@ -0,0 +1,6 @@
+﻿##
+var a := readinteger('Введите кол-во часов: ');
+var b := readinteger('Введите кол-во минут:');
+var c := readinteger('Введите кол-во секунд:');
+var s := a * 3600 + b * 60 + c;
+s.Print
